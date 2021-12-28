9f0d56ba683a85218dc9c11a7cbbbca1c75baad2
Alish-tab <alisharif.tab@gmail.com>
2021-12-28 18:01:40 +0330

    get title of list
    I have added ability for get title of list from user

---
+++ src/list.js
@@ -0,0 +1,32 @@
+const addAnotherList = document.getElementById('add-list__creat')
+
+const inputDiv = document.querySelector('.input-div')
+const listInput = document.getElementById('add-list__input')
+
+const cancelAddList = document.getElementById('add-list__cancel-btn')
+
+
+
+/************************* add another list **********************/
+addAnotherList.addEventListener('click' , () => {
+    if(addAnotherList.classList.contains('visible')){
+        addAnotherList.classList.remove('visible')
+    }
+    addAnotherList.classList.add('hidden')
+    inputDiv.classList.remove('hidden')
+    inputDiv.classList.add('visible')
+
+    
+    
+    
+})
+
+
+/**************************cancel add list **********************/
+cancelAddList.addEventListener('click' , () => {
+    inputDiv.classList.remove('visible')
+    inputDiv.classList.add('hidden')
+    addAnotherList.classList.remove('hidden')
+    addAnotherList.classList.add('visible')
+    
+})
